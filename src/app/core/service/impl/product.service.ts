246f955a780a342fb942ba7cf16b4aae43ef0afd
codehunter <hunterx6323@gmail.com>
2021-05-29 17:03:05 +0700

    connect to real service

---
+++ src/app/core/service/impl/product.service.ts
@@ -0,0 +1,68 @@
+import { HttpClient } from '@angular/common/http';
+import { ElementSchemaRegistry } from '@angular/compiler';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { ProductItem } from '../../model/product-item.model';
+import { ProductStatus } from '../../model/product-status.enum';
+import { ProductType } from '../../model/product-type.enum';
+import { ProductServiceApi } from '../product-service.api';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService extends ProductServiceApi {
+  createProduct(product: ProductItem): Observable<ProductItem> {
+    const body = {
+      description: 'product create from admin page',
+      imageUrl: product.imageUrl,
+      name: product.name,
+      publishDate: product.publishDate,
+      status: ProductStatus[product.status],
+      totalChapter: product.totalChapter,
+      type: ProductType[product.type],
+    };
+    return this.httpClient
+      .post<any>(
+        'https://khong-gian-truyen-backend.herokuapp.com/product',
+        body
+      )
+      .pipe(map((response) => this.convert(response.product)));
+  }
+  getAllProduct(): Observable<ProductItem[]> {
+    return this.httpClient
+      .get<{ productList: any[] }>(
+        'https://khong-gian-truyen-backend.herokuapp.com/product?page=0&size=500&order=BY_ID'
+      )
+      .pipe(
+        map((response) => {
+          const products: ProductItem[] = [];
+          response.productList.forEach((element) => {
+            products.push(this.convert(element));
+          });
+          return products;
+        })
+      );
+  }
+  deleteProduct(id: number): Observable<boolean> {
+    return this.httpClient
+      .delete('https://khong-gian-truyen-backend.herokuapp.com/product/' + id)
+      .pipe(map((response) => true));
+  }
+
+  convert(element: any): ProductItem {
+    return {
+      id: element.id,
+      imageUrl: element.imageUrl,
+      name: element.name,
+      publishDate: element.publishDate,
+      status:
+        ProductStatus[element.status.toString() as keyof typeof ProductStatus],
+      type: ProductType[element.type.toString() as keyof typeof ProductType],
+      totalChapter: element.totalChapter,
+    };
+  }
+  constructor(private httpClient: HttpClient) {
+    super();
+  }
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,36 +1,46 @@
-import { ProductServiceMock } from './../core/service/mock/product-service.mock';
-import { environment } from './../../environments/environment';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { ProductService } from '../core/service/impl/product.service';
 import { ProductServiceApi } from '../core/service/product-service.api';
 import { MaterialModule } from '../material/material.module';
+import { environment } from './../../environments/environment';
+import { ProductServiceMock } from './../core/service/mock/product-service.mock';
 import { HomeComponent } from './home/home.component';
 import { PagesRoutingModule } from './pages-routing.module';
 import { PagesComponent } from './pages.component';
 import { ProductComponent } from './product/product.component';
-import { StoreModule } from '@ngrx/store';
-import * as fromAllProduct from './store/actions/reducers/all-product.reducer';
-import { EffectsModule } from '@ngrx/effects';
 import { AllProductEffects } from './store/actions/effects/all-product.effects';
 import { CreateProductEffects } from './store/actions/effects/create-product.effects';
 import { DeleteProductEffects } from './store/actions/effects/delete-product.effects';
+import * as fromAllProduct from './store/actions/reducers/all-product.reducer';
 
 @NgModule({
   declarations: [PagesComponent, HomeComponent, ProductComponent],
   imports: [
     CommonModule,
+    HttpClientModule,
     PagesRoutingModule,
     MaterialModule,
     FormsModule,
     ReactiveFormsModule,
-    StoreModule.forFeature(fromAllProduct.allProductFeatureKey, fromAllProduct.reducer),
-    EffectsModule.forFeature([AllProductEffects, CreateProductEffects, DeleteProductEffects]),
+    StoreModule.forFeature(
+      fromAllProduct.allProductFeatureKey,
+      fromAllProduct.reducer
+    ),
+    EffectsModule.forFeature([
+      AllProductEffects,
+      CreateProductEffects,
+      DeleteProductEffects,
+    ]),
   ],
   providers: [
     {
       provide: ProductServiceApi,
-      useClass: !environment ? ProductServiceMock : ProductServiceMock,
+      useClass: !environment ? ProductService : ProductService,
     },
   ],
 })
